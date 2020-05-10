7e0c2a83f32a9882a189586f572f45f6e8750abe
Ivan Rubinson <soryy708@gmail.com>
2020-05-10 16:08:23 +0300

    Implement "get all stores" route

--- src/api/routes/index.js
+++ src/api/routes/index.js
@@ -2,11 +2,13 @@ import express from 'express';
 import errorMiddleware from '../middleware/error';
 import frontRoute from './front';
 import authRoute from './auth';
+import storeRoute from './store';
 
 const router = express.Router();
 
 router.use('/front', frontRoute);
 router.use('/auth', authRoute);
+router.use('/store', storeRoute);
 
 // Has to be last:
 router.use(errorMiddleware);
