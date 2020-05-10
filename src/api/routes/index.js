ae1cac6f6ac37cd98b33cc2f20b808e9e14c0cc7
Ivan Rubinson <soryy708@gmail.com>
2020-05-10 17:16:46 +0300

    Implement customer status update route

--- src/api/routes/index.js
+++ src/api/routes/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import errorMiddleware from '../middleware/error';
 import frontRoute from './front';
 import authRoute from './auth';
+import customerRoute from './customer';
 import productRoute from './product';
 import storeRoute from './store';
 
@@ -9,6 +10,7 @@ const router = express.Router();
 
 router.use('/front', frontRoute);
 router.use('/auth', authRoute);
+router.use('/customer', customerRoute);
 router.use('/product', productRoute);
 router.use('/store', storeRoute);
 
